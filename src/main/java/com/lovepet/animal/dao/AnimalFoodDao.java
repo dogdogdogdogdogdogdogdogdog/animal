d37fa82f897b0fd9e2bce703b1875f0410104e88
Lei <meteor790616@gmail.com>
2023-02-02 14:52:42 +0800

    寵物食品-農委會API及測試表格

---
+++ src/main/java/com/lovepet/animal/dao/AnimalFoodDao.java
@@ -0,0 +1,13 @@
+package com.lovepet.animal.dao;
+
+import com.lovepet.animal.dto.AnimalFoodQueryParams;
+import com.lovepet.animal.model.AnimalFood;
+
+import java.util.List;
+
+public interface AnimalFoodDao {
+
+    Integer countAnimalFood(AnimalFoodQueryParams animalFoodQueryParams);
+
+    List<AnimalFood> getAnimalFoods(AnimalFoodQueryParams animalFoodQueryParams);
+}
