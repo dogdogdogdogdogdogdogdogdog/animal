b7ad12b18e3cedd98588c533712a7739e7c1cfaa
Dorisliu18 <sugar840318@gmail.com>
2023-02-10 14:00:35 +0800

    動物醫院後端資料

--- src/main/java/com/lovepet/animal/dao/impl/AnimalHospitalDaoImpl.java
+++ src/main/java/com/lovepet/animal/dao/impl/AnimalHospitalDaoImpl.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class AnimalHospitalDaoImpl implements AnimalHospitalDao {
@@ -17,20 +19,58 @@ public class AnimalHospitalDaoImpl implements AnimalHospitalDao {
     private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     @Override
-    public Integer countAnimalHospital(AnimalHospitalQueryParams animalHospitalQueryParams) {
-        return null;
-    }
-
-    @Override
-    public List<AnimalHospital> getAnimalHospitals(AnimalHospitalQueryParams animalHospitalQueryParams) {
-        return null;
-    }
-
-    @Override
     public List<AnimalHospital> getAnimalHospitalsComboBox(){
         String sql = "SELECT * FROM animal_hospital WHERE 1=1";
 
         List<AnimalHospital> list = namedParameterJdbcTemplate.query(sql , new AnimalHospitalRowmapper());
         return list;
     }
+
+    @Override
+    public Integer countAnimalHospital(AnimalHospitalQueryParams animalHospitalQueryParams) {
+        String sql = "SELECT count(*) FROM animal_hospital WHERE 1=1";
+
+        Map<String, Object> map = new HashMap<>();
+
+        //查詢條件
+        sql = addFilteringSql(sql, map, animalHospitalQueryParams);
+
+        //將 count 值轉換為 Integer 類型的返回值
+        Integer total = namedParameterJdbcTemplate.queryForObject(sql, map, Integer.class);
+
+        return total;
+    }
+
+    @Override
+    public List<AnimalHospital> getAnimalHospitals(AnimalHospitalQueryParams animalHospitalQueryParams) {
+        String sql = "SELECT area, sn, license, business, hospital, principal, " +
+                "tel, date_of_issue, address FROM animal_hospital WHERE 1=1";
+
+        Map<String, Object> map = new HashMap<>();
+
+        // 查詢條件
+        sql = addFilteringSql(sql, map, animalHospitalQueryParams);
+
+        //分頁
+        sql = sql + " LIMIT :limit OFFSET :offset";
+        map.put("limit", animalHospitalQueryParams.getLimit());
+        map.put("offset", animalHospitalQueryParams.getOffset());
+
+        List<AnimalHospital> animalHospitalList = namedParameterJdbcTemplate.query(sql, map, new AnimalHospitalRowmapper());
+
+        return animalHospitalList;
+    }
+
+    private String addFilteringSql(String sql, Map<String, Object> map, AnimalHospitalQueryParams animalHospitalQueryParams) {
+        if(animalHospitalQueryParams.getArea() != null){
+            sql = sql + " AND area = :area";
+            map.put("area", animalHospitalQueryParams.getArea());
+        }
+        if (animalHospitalQueryParams.getSearch() !=null) {
+            sql = sql + " AND name LIKE :search";
+            map.put("search", "%" + animalHospitalQueryParams.getSearch() + "%");
+        }
+        return  sql;
+    }
+
 }
