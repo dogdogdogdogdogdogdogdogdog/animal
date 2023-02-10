fc2d42442f755aa91d69af930965c611b1fb34d0
max7789632 <max7789632@gmail.com>
2023-02-10 15:26:40 +0800

    論壇系統 前端版面暫定

--- src/main/animal/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.lovepet.animal.dao;
-
-import com.lovepet.animal.dto.UserRegisterRequest;
-
-public interface UserDao {
-    public Integer createUser(UserRegisterRequest userRegisterRequest);
-}
