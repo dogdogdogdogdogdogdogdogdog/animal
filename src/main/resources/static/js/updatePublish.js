f2f58916c5f59c0690caa081c89a55cf6d982ad3
Lei <meteor790616@gmail.com>
2023-02-19 20:50:36 +0800

    送養管理完成 修改避免快取獲得圖片

--- src/main/resources/static/js/updatePublish.js
+++ src/main/resources/static/js/updatePublish.js
@@ -1,33 +1,34 @@
+function updatePublish() {
 
-function updatePublish(){
-
-    var infoModal= document.getElementById("infoModal");
-    var  inp=  infoModal.getElementsByTagName('input')
-    var fd=new FormData();
-    let c=0
-    for(let i=0;i<inp.length;i++){
-        if(inp[i].value!=""){
-            if(inp[i].id=="animalPhoto"){
-                fd.append(inp[i].id,inp[i].files[0])
+    var infoModal = document.getElementById("infoModal");
+    var inp = infoModal.getElementsByTagName('input')
+    var fd = new FormData();
+    let c = 0
+    for (let i = 0; i < inp.length; i++) {
+        if (inp[i].value != "") {
+            if (inp[i].id == "animalPhoto") {
+                fd.append(inp[i].id, inp[i].files[0])
             }
-            fd.append(inp[i].id,inp[i].value)
+            fd.append(inp[i].id, inp[i].value)
             c++
         }
     }
-    if (c==0){alert('請輸入修改值')
-            return}
-    fd.append("userId",user.id)
-    fd.append("item",document.getElementById("infoModal").className)
-    fd.append("imageUrl",'/static/images/publish/')
+    if (c == 0) {
+        alert('請輸入修改值')
+        return
+    }
+    fd.append("userId", user.id)
+    fd.append("item", document.getElementById("infoModal").className)
+    fd.append("imageUrl", '/static/images/publish/')
 
 
-    var xhr=new XMLHttpRequest()
-    xhr.addEventListener("load", function (){
+    var xhr = new XMLHttpRequest()
+    xhr.addEventListener("load", function () {
 
 
-        if(xhr.status==200){
-            for(let i=0;i<inp.length;i++) {
-                inp[i].value==""
+        if (xhr.status == 200) {
+            for (let i = 0; i < inp.length; i++) {
+                inp[i].value == ""
             }
             alert('修改成功')
             window.location.reload();
